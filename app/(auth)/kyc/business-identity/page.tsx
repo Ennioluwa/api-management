bf78ce323dfcf2762a73824389a82b3c7190336b
Ennyoluwa <enioluwaarowo@gmail.com>
2024-02-02 04:52:20 +0100

    change name

--- app/(auth)/kyc/business-identity/page.tsx
+++ app/(auth)/kyc/business-identity/page.tsx
@@ -1,8 +1,12 @@
+"use client";
+
 import Navbar from "../_components/Navbar";
 import BusinessIdentityForm from "./_components/BusinessIdentityForm";
 import Authenticated from "./_components/Authenticated";
+import { useAppSelector } from "@/lib/hooks";
 
 const page = ({}) => {
+  const { userData } = useAppSelector((state) => state.user);
   return (
     <Authenticated>
       <div>
@@ -11,7 +15,7 @@ const page = ({}) => {
           <div className="container max-w-[440px] p-8 border rounded-lg space-y-8 ">
             <div>
               <h3 className=" text-xl font-bold mb-2.5">
-                Welcome Nusaiba, Tell us about your company
+                Welcome {userData?.firstName}, Tell us about your company
               </h3>
               <h4 className=" text-xs mb-2.5">
                 Enter appropriate and official information about your business
--- app/(auth)/kyc/business-information/page.tsx
+++ app/(auth)/kyc/business-information/page.tsx
@@ -1,8 +1,12 @@
+"use client";
+
+import { useAppSelector } from "@/lib/hooks";
 import Navbar from "../_components/Navbar";
 import Authenticated from "./_components/Authenticated";
 import BusinessInformationForm from "./_components/BusinessInformationForm";
 
 const page = ({}) => {
+  const { userData } = useAppSelector((state) => state.user);
   return (
     <Authenticated>
       <div className=" overflow-hidden">
@@ -11,7 +15,7 @@ const page = ({}) => {
           <div className="container max-w-[440px] p-8 border rounded-lg space-y-8 overflow-auto ">
             <div>
               <h3 className=" text-xl font-bold mb-2.5">
-                Welcome Nusaiba, Tell us about your company
+                Welcome {userData?.firstName}, Tell us about your company
               </h3>
               <h4 className=" text-xs mb-2.5">
                 Enter appropriate and official information about your business
