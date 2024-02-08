2ec1a5a6214775d14a6b9ea603a5263bad422370
Ennyoluwa <enioluwaarowo@gmail.com>
2024-02-08 06:02:30 +0100

    modify invoice glance

--- app/dashboard/invoices/_components/InvoiceGlance.tsx
+++ app/dashboard/invoices/_components/InvoiceGlance.tsx
@@ -36,7 +36,7 @@ const UserRoles: FC<UserRolesProps> = ({}) => {
         An intuitive way to see all your general invoices for a quick access
       </p>
       <div className="flex flex-col md:flex-row flex-wrap md:items-stretch gap-4">
-        <div className=" p-5  border border-dashed border-[#9A9AAF] rounded-lg md:flex-[2] flex flex-col sm:flex-row gap-4 justify-between items-start sm:items-center">
+        <div className=" p-5  border border-dashed border-[#9A9AAF] rounded-lg md:flex-[2] grid grid-cols-3 gap-3">
           <div className="space-y-1.5">
             <h6 className=" text-xs font-bold">TOTAL INVOICES</h6>
             <h4 className=" text-3xl font-bold">
@@ -46,76 +46,73 @@ const UserRoles: FC<UserRolesProps> = ({}) => {
               + {4}% compared to last month
             </p> */}
           </div>
-          <div className=" space-y-3">
-            <div className=" px-3 py-1.5 rounded-lg space-y-1 bg-[#9A9AAF]/20">
-              <p className=" flex items-center gap-1 text-xs font-bold text-[#9a9aaf]">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="12"
-                  height="10"
-                  viewBox="0 0 12 10"
-                  fill="none"
-                >
-                  <ellipse
-                    cx="5.99989"
-                    cy="5"
-                    rx="5.55556"
-                    ry="5"
-                    fill="#9A9AAF"
-                  />
-                </svg>
-                <span>THIS MONTH</span>
-              </p>
-              <p className=" text-xs font-bold">
-                {invoiceStats?.currentMonthProcessed}
-              </p>
-            </div>
-            <div className=" px-3 py-1.5 rounded-lg space-y-1 bg-[#9A9AAF]/20">
-              <p className=" flex items-center gap-1 text-xs font-bold text-[#0062FF] uppercase">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="12"
-                  height="10"
-                  viewBox="0 0 12 10"
-                  fill="none"
-                >
-                  <ellipse
-                    cx="5.99989"
-                    cy="5"
-                    rx="5.55556"
-                    ry="5"
-                    fill="#0062FF"
-                  />
-                </svg>
-                <span>LAST MONTH</span>
-              </p>
-              <p className=" text-xs font-bold">
-                {invoiceStats?.lastMonthProcessed}
-              </p>
-            </div>
+          <div className=" px-3 rounded-lg space-y-1 bg-[#9A9AAF]/20 w-full flex flex-col justify-center">
+            <p className=" flex items-center gap-1 text-xs font-bold text-[#9a9aaf]">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="12"
+                height="10"
+                viewBox="0 0 12 10"
+                fill="none"
+              >
+                <ellipse
+                  cx="5.99989"
+                  cy="5"
+                  rx="5.55556"
+                  ry="5"
+                  fill="#9A9AAF"
+                />
+              </svg>
+              <span>THIS MONTH</span>
+            </p>
+            <p className=" text-xs font-bold">
+              {invoiceStats?.currentMonthProcessed}
+            </p>
+          </div>
+          <div className=" px-3 rounded-lg space-y-1 bg-[#9A9AAF]/20 w-full h-full flex flex-col justify-center">
+            <p className=" flex items-center gap-1 text-xs font-bold text-[#0062FF] uppercase">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="12"
+                height="10"
+                viewBox="0 0 12 10"
+                fill="none"
+              >
+                <ellipse
+                  cx="5.99989"
+                  cy="5"
+                  rx="5.55556"
+                  ry="5"
+                  fill="#0062FF"
+                />
+              </svg>
+              <span>LAST MONTH</span>
+            </p>
+            <p className=" text-xs font-bold">
+              {invoiceStats?.lastMonthProcessed}
+            </p>
           </div>
         </div>
-        <div className="flex-1 flex flex-col md:flex-row  md:items-stretch gap-4">
-          <div className="p-5 border border-dashed border-[#9A9AAF] rounded-lg flex-1 space-y-1.5">
-            <h6 className=" text-xs font-bold">PENDING</h6>
-            <h4 className=" text-3xl font-bold flex gap-2.5 items-baseline">
-              {invoiceStats?.pendingInvoice}{" "}
-              <span className=" text-[#FFCF5C] text-base ">↑0.7%</span>
-            </h4>
-            {/* <p className=" text-xs font-bold uppercase text-[#FFCF5C] px-2.5 py-1 rounded-r-full rounded-l-full bg-[#FFCF5C]/10">
+
+        <div className="p-5 border border-dashed border-[#9A9AAF] rounded-lg flex-1 space-y-1.5">
+          <h6 className=" text-xs font-bold">PENDING</h6>
+          <h4 className=" text-3xl font-bold flex gap-2.5 items-baseline">
+            {invoiceStats?.pendingInvoice}{" "}
+            <span className=" text-[#FFCF5C] text-base ">↑0.7%</span>
+          </h4>
+          {/* <p className=" text-xs font-bold uppercase text-[#FFCF5C] px-2.5 py-1 rounded-r-full rounded-l-full bg-[#FFCF5C]/10">
               + {4}% compared to last month
             </p> */}
-          </div>
-          <div className="p-5 border border-dashed border-[#9A9AAF] rounded-lg flex-1 space-y-1.5">
-            <h6 className=" text-xs font-bold">FAILED</h6>
-            <h4 className=" text-3xl font-bold flex gap-2.5 items-baseline">
-              {invoiceStats?.failedInvoice}{" "}
-              <span className=" text-[#A71C1C] text-base ">↑0.7%</span>
-            </h4>
-            {/* <p className=" w-fit text-xs font-bold uppercase text-[#A71C1C] px-2.5 py-1 rounded-r-full rounded-l-full bg-[#A71C1C]/10">
+        </div>
+        <div className="p-5 border border-dashed border-[#9A9AAF] rounded-lg flex-1 space-y-1.5">
+          <h6 className=" text-xs font-bold">FAILED</h6>
+          <h4 className=" text-3xl font-bold flex gap-2.5 items-baseline">
+            {invoiceStats?.failedInvoice}{" "}
+            <span className=" text-[#A71C1C] text-base ">↑0.7%</span>
+          </h4>
+          {/* <p className=" w-fit text-xs font-bold uppercase text-[#A71C1C] px-2.5 py-1 rounded-r-full rounded-l-full bg-[#A71C1C]/10">
               + 16% compared to last week
             </p> */}
-          </div>
         </div>
       </div>
     </div>
