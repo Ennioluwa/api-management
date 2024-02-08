5f5a72488dadca60aec2aea19df6a69fe5eee9bb
Ennyoluwa <enioluwaarowo@gmail.com>
2024-02-08 05:30:21 +0100

    add navbar click

--- app/dashboard/_components/Navbar.tsx
+++ app/dashboard/_components/Navbar.tsx
@@ -3,35 +3,65 @@
 import Logo from "@/app/(landing)/_components/Logo";
 import { Input } from "@/components/ui/input";
 import { useAppSelector } from "@/lib/hooks";
-import { HambergerMenu, Message, Notification } from "iconsax-react";
+import { Messages1, NotificationBing } from "iconsax-react";
 import { FC } from "react";
 import { Search } from "./SearchInput";
 import IsAdminAuth from "@/components/isAdminAuth";
 import { MobileSidebar } from "./MobileSidebar";
+import { Button } from "@/components/ui/button";
+import { Label } from "@/components/ui/label";
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
+import Link from "next/link";
 
 interface NavbarProps {}
 
 const Navbar: FC<NavbarProps> = ({}) => {
   const { userData } = useAppSelector((state) => state.user);
+
+  const handleUserOpen = () => {};
   return (
     <nav className="fixed top-0 left-0 right-0 w-full h-16  px-3 bg-white border-b z-40 shadow grid place-items-center">
-      <div className="flex items-center justify-between w-full gap-5">
-        <div className=" flex items-center gap-3">
+      <div className="flex items-center justify-between w-full gap-5 md:gap-12">
+        <div className=" flex items-center gap-3 shrink-0">
           <MobileSidebar />
           {/* <HambergerMenu size={30} className=" cursor-pointer" /> */}
           <Logo />
         </div>
 
-        {/* <div className=" grow">
-          <Search className=" max-w-80 " />
-        </div> */}
+        <div className=" grow hidden md:block">
+          <Search className="" />
+        </div>
         <div className=" flex items-center gap-5">
-          {/* <Message /> */}
-          <Notification />
-          <div className=" flex flex-col">
-            <p>{userData?.firstName}</p>
-            <p>{userData?.roles[0] ? userData?.roles[0] : "Member"}</p>
-          </div>
+          <Messages1 variant="Bulk" size={30} />
+          <NotificationBing variant="Bulk" size={30} />
+          <Popover>
+            <PopoverTrigger asChild>
+              <div className=" flex flex-col cursor-pointer">
+                <p>{userData?.firstName}</p>
+                <p>{userData?.roles[0] ? userData?.roles[0] : "Member"}</p>
+              </div>
+            </PopoverTrigger>
+            <PopoverContent
+              align="end"
+              className="w-full p-3 flex flex-col gap-2 bg-white"
+            >
+              <Link href="/dashboard/settings" className=" w-full">
+                <Button variant="ghost" className=" w-full">
+                  Edit Account
+                </Button>
+              </Link>
+              <hr className=" border-dashed border-[#9A9AAF] " />
+              <Link href="/login" className=" w-full">
+                <Button variant="ghost" className=" w-full">
+                  Log Out
+                </Button>
+              </Link>
+            </PopoverContent>
+          </Popover>
         </div>
       </div>
     </nav>
