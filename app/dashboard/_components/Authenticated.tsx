e32b2c3a671d5af6ab48d6d98a4b28d7f468dd93
Ennyoluwa <enioluwaarowo@gmail.com>
2024-02-10 05:45:21 +0100

    add auth

--- app/dashboard/_components/Authenticated.tsx
+++ app/dashboard/_components/Authenticated.tsx
@@ -4,7 +4,7 @@ import IsAdminAuth from "@/components/isAdminAuth";
 import { useAppSelector } from "@/lib/hooks";
 import { fetchUsers } from "@/lib/hooks/api/users.api";
 import { useQuery } from "@tanstack/react-query";
-import { redirect } from "next/navigation";
+import { redirect, usePathname, useRouter } from "next/navigation";
 import { FC, ReactNode, useLayoutEffect } from "react";
 
 interface AuthenticatedProps {
@@ -14,29 +14,27 @@ interface AuthenticatedProps {
 const Authenticated: FC<AuthenticatedProps> = ({ children }) => {
   const { userData } = useAppSelector((state) => state.user);
 
-  const {
-    isPending,
-    isError,
-    data: users,
-    error,
-    refetch,
-  } = useQuery({
-    queryKey: ["users"],
-    queryFn: fetchUsers,
-  });
+  const pathName = usePathname();
 
-  // useLayoutEffect(() => {
-  //   if (!userData) {
-  //     console.log("no user");
-  //     return redirect("/login");
-  //   } else if (userData.setupStatus === "AccountCreated") {
-  //     return redirect("/kyc/business-information");
-  //   } else if (userData.setupStatus === "CompanyCreated") {
-  //     return redirect("/kyc/business-identity");
-  //   } else if (userData.changePassword === true) {
-  //     return redirect("/change-password");
-  //   } else return;
-  // }, []);
+  useLayoutEffect(() => {
+    if (!userData) {
+      console.log("no user");
+      return redirect("/login");
+    } else if (userData.setupStatus === "AccountCreated") {
+      return redirect("/kyc/business-information");
+    } else if (userData.setupStatus === "CompanyCreated") {
+      return redirect("/kyc/business-identity");
+    } else if (userData.changePassword === true) {
+      return redirect("/change-password");
+    } else if (
+      userData.companyStatus === "Pending" &&
+      pathName !== "/dashboard/home" &&
+      pathName !== "/dashboard/subscription"
+    ) {
+      redirect("/dashboard/home");
+    } else return;
+  }, [pathName]);
+
   return <>{children}</>;
 };
 
